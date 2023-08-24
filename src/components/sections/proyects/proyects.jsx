5ca2f8d26a97e443e874b75364be22b2199f8762
AugustoCorbalan <auguscorbi@gmail.com>
2023-08-24 19:12:30 -0300

    style improvements

--- src/components/sections/proyects/proyects.jsx
+++ src/components/sections/proyects/proyects.jsx
@@ -3,7 +3,14 @@ import styles from './proyects.module.css';
 export const Proyects = ()=>{
     return(
         <div className={styles.principalContainer}>
-            <CarruselCards/>
+            <div className={styles.container}>
+                <div className={styles.containerTitle}>
+                    <h2>Proyectos:</h2>
+                </div>
+                <div className={styles.containerProyects}>
+                    <CarruselCards/>
+                </div>
+            </div>
         </div>
     )
 }
