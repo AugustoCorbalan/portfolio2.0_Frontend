35580cf7fb5429d56dac7b939fff586a69db53c1
AugustoCorbalan <auguscorbi@gmail.com>
2023-08-27 21:27:05 -0300

    plot initial animation

--- src/components/initial_animation/initialAnimation.jsx
+++ src/components/initial_animation/initialAnimation.jsx
@@ -1,10 +1,12 @@
 import styles from './initialAnimation.module.css';
 import { Icon } from './components/icon/icon.jsx';
+import { Plot } from './components/plot/plot';
 
 export const InitialAnimation = ()=>{
     return(
         <div className={styles.container}>
             <Icon/>
+            <Plot/>
         </div>
     )
 }
