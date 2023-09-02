75f24e1a5d11885aa5f25a7e206c468ceedc0235
AugustoCorbalan <auguscorbi@gmail.com>
2023-09-02 19:24:48 -0300

    add curriculum to the navBar

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,13 +1,23 @@
 import styles from './navBar.module.css';
+import { myData } from '../../data/my_data.js';
 
 export const NavBar = ()=>{
     return(
         <div className={styles.container}>  
             <div className={styles.subContainer1}>
+                
+            </div>
+            <div className={styles.subContainer2}>
                 <button href="/" className={styles.link}>PRINCIPAL</button>
                 <button href="/" className={styles.link}>SOBRE_MI</button>
                 <button href="/" className={styles.link}>PROYECTOS</button>
                 <button href="/" className={styles.link}>TECNOLOGÍAS</button>
+            </div>
+            <div className={styles.subContainer3}>
+                <a href={myData.CV.link} className={styles.container_cv}>
+                    <img src={myData.CV.icon} alt={myData.CV.name} />
+                    <h2>Curriculum</h2>
+                </a>
             </div>
         </div>
     )
--- src/data/my_data.js
+++ src/data/my_data.js
@@ -1,10 +1,12 @@
 import linkedin_icon from '../assets/images/iconsLinks/linkedin.png';
 import github_icon from '../assets/images/iconsLinks/github.png';
 import gmail_icon from '../assets/images/iconsLinks/gmail.png';
+import google_docs from '../assets/images/iconsLinks/google_docs.svg';
 export const myData = {
     contact: [
         {name:"linkedin", icon: linkedin_icon , link: "https://www.linkedin.com/in/augusto-corbalan-a6b857256/"},
         {name:"gitHub", icon:github_icon, link:"https://github.com/AugustoCorbalan"},
-        {name:"mail", icon:gmail_icon, link:"corbalan.augusto.n@gmail.com"}
-    ]
+        {name:"mail", icon:gmail_icon, link:"corbalan.augusto.n@gmail.com"},
+    ],
+    CV: {name:"curriculum vitae", icon:google_docs, link:"https://drive.google.com/file/d/1YvsBJRnoUGItfQubDGLZJzzdiJAX3i0c/view?usp=drive_link"}
 }
